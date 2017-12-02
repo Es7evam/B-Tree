16b092f1f36b6f8ad457d1c7e9589fb8cdb4adc8
Es7evam <estevam.arantes@gmail.com>
2017-12-02 14:36:18 +0000

    Add prototype of menu at main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@
 int main(void) { 
     long offset = 0;
     char buff[1000] = {};
-    FILE *ceps = NULL;
+    FILE *DATA_FILE = NULL;
     FILE *consultas = NULL;
     tRegistro temp;
     Arvore arvore;
+
+    DATA_FILE = fopen("dados.dat", "rb"); //ajeitar nome arquivo
+    if(DATA_FILE == NULL){
+        cout << "Arquivo de dados inexistente!" << endl;
+        cout << "Saindo do programa..." << endl;
+        return -1;
+    }
 
     arvore_iniciar(&arvore);
     
@@ -14,16 +21,57 @@ int main(void) {
     if(arvore.fp == NULL)
         arvore.fp = fopen("indices.dat", "wb+"); //ajeitar nome arquivo (talvez rb+)
 
-    ceps = fopen("dados.dat", "rb"); //ajeitar nome arquivo
+
+    int idBusca;
+    cout << "Menu\n";
+    cout << "1) Criar Índice\n";
+    cout << "2) Inserir Música\n";
+    cout << "3) Pesquisar Música por ID\n";
+    cout << "4) Remover Música por ID\n";
+    cout << "5) Mostrar Árvore-B\n";
+    cout << "6) Fechar o programa";
+    bool stay = true;
+    while(stay){
+        int opt;
+        cin >> opt;
+        switch(opt){
+    
+           case 1: 
+                criar_Indice();
+                arvore_iniciar();
+                break;
+    
+           case 2:
+                inserir();
+                break;
+            
+            case 3:
+                cin >> idBusca;
+                busca(Arvore *arv, int idBusca);
+                break;
+            
+            case 4:
+                //a implementar
+                break;
+            
+            case 5:
+                break;
+            
+            case 6:
+                stay = false;
+                break;
+        }
+    }
+
 
     //MONTANDO ARVORE
-    while(!feof(ceps)) { // 0 se final do arquivo
-        offset = ftell(ceps);
+    while(!feof(DATA_FILE)) { // 0 se final do arquivo
+        offset = ftell(DATA_FILE);
         int ids;
         string title;
         string gen;
 
-        fgets(buff, 1000, ceps);
+        fgets(buff, 1000, DATA_FILE);
         
         memcpy(temp.CEP, buff, 8);
         
@@ -55,14 +103,14 @@ int main(void) {
             printf("%s NAO ENCONTRADO\n", buff);
         }
         else {
-            fseek(ceps, offset, SEEK_SET);
-            fgets(buff, 1000, ceps);
+            fseek(DATA_FILE, offset, SEEK_SET);
+            fgets(buff, 1000, DATA_FILE);
             LIMPAR_QUEBRA(buff);
             printf("%s\n", buff);
         }
     }
     fclose(arvore.fp);
     fclose(consultas);
-    fclose(ceps);
+    fclose(DATA_FILE);
     return 0;
 }
